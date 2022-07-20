f73e28457f65650cc685082f0a25962389938da5
Michael <Michael@imacdecapucine.home>
2022-07-20 17:54:44 +0200

    ajout axios

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
+import AboutViewVue from '@/views/AboutView.vue'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductsViewVue from '../views/ProductsView.vue'
 
 Vue.use(VueRouter)
 
@@ -16,7 +18,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutViewVue
+  },
+  {
+    path: '/products',
+    name: 'products',
+    component: ProductsViewVue
   }
 ]
 
